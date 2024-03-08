75abef1564b4f916aad965b4e84a2b887e65b5ec
redaelhoumadi <reda.elhoumadi@gmail.com>
2024-03-08 16:10:01 +0100

    add sidebar menu

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,13 @@
+import SideBarMenu from "./sidebar_menu"
+import '../styles/sidebar.css'
+function SideBar(){
+return (
+    <div className="sidebar">
+    <SideBarMenu />
+    
+    </div>
+    
+)
+}
+
+export default SideBar
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,13 @@
+import { Fragment } from 'react'
 import Header from '../components/header'
+import SideBar from '../components/sidebar'
 function Index() {
     return(
+        <Fragment>
         <Header />
+        <SideBar />
+        </Fragment>
+        
     )
 }
 
